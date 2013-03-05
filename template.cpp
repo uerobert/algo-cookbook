a91e615b51be9c0726e9ee574339a29d28c85d23
Robert Ubiñas <ubinas.r56@gmail.com>
2013-03-04 23:10:21 -0400

    added a template

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <utility>
+#include <complex>
+#include <bitset>
+#include <limits>
+#include <vector>
+#include <list>
+#include <deque>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <cctype>
+#include <cstdlib>
+
+#define INF 1e9
+#define PI 3.14159265359
+
+#define pb push_back
+#define mp make_pair
+
+#define ZERO(X) memset(X,0,sizeof(X))
+
+typedef long long LL;
